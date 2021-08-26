046c98f65ba8a0e70baa225301189a6da56755a5
Paulo Flóra <tflora.2004@gmail.com>
2021-08-26 18:37:38 -0300

    Feito o requisito 21.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const email = document.getElementById('email');
+const userEmail = document.getElementById('email');
 const password = document.getElementById('password');
 const submitHeaderBtn = document.getElementById('submit');
 const submitFormBtn = document.getElementById('submit-btn');
 const checkAgreement = document.getElementById('agreement');
-let textCounter = document.getElementById('counter');
-let textArea = document.getElementById('textarea');
-let inputs = document.getElementsByClassName('subject')
-parseInt(textCounter.innerText)
+const textCounter = document.getElementById('counter');
+const textArea = document.getElementById('textarea');
+const form = document.getElementById('evaluation-form');
+// const inputs = document.getElementsByClassName('subject');
 
 function checkUser(event) {
   event.preventDefault();
-  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+  if (userEmail.value === 'tryber@teste.com' && password.value === '123456') {
     window.alert('Olá, Tryber!');
   } else {
     window.alert('Email ou senha inválidos.');
@@ -28,6 +28,40 @@ function submitForm() {
 
 checkAgreement.addEventListener('click', submitForm);
 
-textArea.addEventListener('keyup', function() {
+function updateCounter() {
   textCounter.innerText = 500 - textArea.value.length;
-})
+}
+
+textArea.addEventListener('input', updateCounter);
+
+function getSubjects() {
+  let subjectsText = 'Materias: ';
+  const subjects = document.querySelectorAll('input[class="subject"]:checked');
+  for (let i = 0; i < subjects.length; i += 1) {
+    subjectsText += `${subjects[i].value} `;
+  }
+  return subjectsText;
+}
+
+function getFormData() {
+  const name = document.getElementById('input-name').value;
+  const lastName = document.getElementById('input-lastname').value;
+  const fullName = `Nome: ${name} ${lastName}`;
+  const email = document.getElementById('input-email').value;
+  const house = document.getElementById('house').value;
+  const family = document.querySelector('input[name="family"]:checked').value;
+  const subjects = getSubjects();
+  const evaluation = document.querySelector('input[name="rate"]:checked').value;
+  const observations = document.querySelector('textarea').value;
+
+  form.innerHTML = '';
+  form.innerHTML = `${fullName}<br>
+    Email: ${email}<br>
+    Casa: ${house}<br>
+    Família: ${family}<br>
+    ${subjects}<br>
+    Avaliação: ${evaluation}<br>
+    Observações: ${observations}`;
+}
+
+submitFormBtn.addEventListener('click', getFormData);
